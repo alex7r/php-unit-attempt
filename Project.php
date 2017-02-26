752aefd00d2b9a876d7c5b61287673f152bcdaad
Alexandr Kosarev <a.kosarev@belitsoft.com>
2017-02-26 17:13:17 +0300

    source files

---
+++ Project.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) 2017. Alexandr Kosarev (http://kosarev.by)
+ */
+
+/**
+ * Created by Alexandr @ PhpStorm.
+ * Date: 26.02.2017 14:35
+ */
+
+namespace System;
+
+
+class Project
+{
+    public $id;
+    public $title;
+    public $url;
+    public $published;
+
+    protected function __construct($title, $url, $published)
+    {
+        $this->id = rand(0,100);
+        $this->title = $title;
+        $this->url = $url;
+        $this->published = $published;
+    }
+
+    public static function createProject($title, $url, $published = 0){
+        if(preg_match('/https?:\/\/(?:[^.]*?\.){1,}(?:[^.]*)/i',$url)){
+            $instance = new Project($title, $url, $published);
+            return $instance;
+        }
+        return null;
+    }
+
+}
---
+++ starter.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright (c) 2017. Alexandr Kosarev (http://kosarev.by)
+ */
+
+/**
+ * Created by Alexandr @ PhpStorm.
+ * Date: 26.02.2017 15:01
+ */
+include_once 'project.php';
+$project = System\Project::createProject('My project','http://kosarev.by',1);
+var_dump($project);
