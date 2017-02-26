59afe26ccf6c7c48d769092ab30db2b152740616
Alexandr Kosarev <a.kosarev@belitsoft.com>
2017-02-26 17:50:45 +0300

    successful test

---
+++ tests/ProjectTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) 2017. Alexandr Kosarev (http://kosarev.by)
+ */
+
+/**
+ * Created by Alexandr @ PhpStorm.
+ * Date: 26.02.2017 15:52
+ */
+
+namespace System;
+
+class ProjectTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testProjectCreation()
+    {
+        $project = Project::createProject('MyTitle','http://kosarev.by');
+        $bad_project = Project::createProject('MyTitle','http://by');
+
+        $this->assertInstanceOf(Project::class, $project);
+        $this->assertNull($bad_project);
+
+    }
+}
